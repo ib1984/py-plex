f3b5530a14ba7ac74f9b4f0f2b544ade78283267
Ivan Butorin <ivan.butorin@gmail.com>
2014-08-22 15:25:28 +0700

    Add 'season' property to episode

--- episode.py
+++ episode.py
@@ -8,12 +8,13 @@ class Episode(Video):
         self.type = 'episode'
         try:
             self.index = int(element.attrib['index'])
+            self.season = int(element.attrib['parentIndex'])
         except KeyError as e:
             print "Missing key in element: ", e.message
 
     def __str__(self):
-        return "<Episode: %s (%d)>" % (self.title, self.index)
+        return "<Episode: %s (%dx%.2d)>" % (self.title, self.season, self.index)
 
     def __repr__(self):
-        return "<Episode: %s (%d)>" % (self.title, self.index)
+        return "<Episode: %s (%dx%.2d)>" % (self.title, self.season, self.index)
 
